0283af5d60a8aab1c3876ba3df3719076e5dbb17
omer mohamed <damouramory@gmail.com>
2024-11-27 20:53:10 +0200

    chore: add footer component to test page

--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -6,8 +6,8 @@ import Link from "next/link";
 
 import { motion, useAnimation } from "framer-motion";
 
-import { useInView } from "react-intersection-observer";
 import { useState } from "react";
+import classNames from "classnames";
 
 interface link {
   label: string;
@@ -63,10 +63,16 @@ export default function Navbar({
           viewBox="0 0 50 50"
           fill="white"
           // style={"fill:#FFFFFF;"
-          className={styles.burgerMenu}
+          className={classNames(
+            styles.burgerMenu,
+            styles[`${isOpen ? "burgerMenu--open" : undefined}`]
+          )}
           onClick={() => setIsOpen(!isOpen)}
         >
-          <path d="M 3 9 A 1.0001 1.0001 0 1 0 3 11 L 47 11 A 1.0001 1.0001 0 1 0 47 9 L 3 9 z M 3 24 A 1.0001 1.0001 0 1 0 3 26 L 47 26 A 1.0001 1.0001 0 1 0 47 24 L 3 24 z M 3 39 A 1.0001 1.0001 0 1 0 3 41 L 47 41 A 1.0001 1.0001 0 1 0 47 39 L 3 39 z"></path>
+          <path
+            className="path"
+            d="M 3 9 A 1.0001 1.0001 0 1 0 3 11 L 47 11 A 1.0001 1.0001 0 1 0 47 9 L 3 9 z M 3 24 A 1.0001 1.0001 0 1 0 3 26 L 47 26 A 1.0001 1.0001 0 1 0 47 24 L 3 24 z M 3 39 A 1.0001 1.0001 0 1 0 3 41 L 47 41 A 1.0001 1.0001 0 1 0 47 39 L 3 39 z"
+          ></path>
         </svg>
       </motion.nav>
     </>
