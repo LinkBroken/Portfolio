5b49f8d0c6f881a9229121347410e8ec389b9325
omer mohamed <damouramory@gmail.com>
2024-11-28 21:59:18 +0200

    feat: add optional padding for the container and title for Title component

--- app/components/Title/TItle.tsx
+++ app/components/Title/TItle.tsx
@@ -4,18 +4,34 @@ import styles from "./title.module.css";
 interface TitleProps {
   label: string;
   border: boolean;
+  containerPadding: boolean;
+  titlePadding: boolean;
   size: "small" | "medium" | "large";
   align: "left" | "right" | "center";
 }
 
-export default function Title({ align, label, size, border }: TitleProps) {
+export default function Title({
+  align,
+  containerPadding,
+  titlePadding,
+  label,
+  size,
+  border,
+}: TitleProps) {
   return (
-    <div className={styles.titleContainer}>
+    <div
+      className={classNames(styles.titleContainer, {
+        [styles[`container--${containerPadding ? "padding" : "nopadding "}`]]:
+          containerPadding,
+      })}
+    >
       <h1
         className={classNames(styles.title, {
           [styles[`title--${size}`]]: size,
           [styles[`title--${align}`]]: align,
           [styles[`title--${border ? "border" : "borderless "}`]]: border,
+          [styles[`title--${titlePadding ? "padding" : "nopadding "}`]]:
+            titlePadding,
         })}
       >
         {label}
