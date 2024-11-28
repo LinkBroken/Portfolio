1dc07ddd709fc605a71ee6339cbd986f17f334b3
omer mohamed <damouramory@gmail.com>
2024-11-28 22:24:05 +0200

    feat: adding content block component for test page

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,14 @@ import user from "@/assets/userImage.png";
 import logo from "@/assets/logo.svg";
 import { navLinks } from "@/stories/components/Navbar/Navbar.stories";
 import Footer from "./components/Footer/Footer";
+import Title from "./components/Title/TItle";
+import { motion, useScroll, useSpring } from "framer-motion";
+import ContentBlock from "./components/ContentBlock/ContentBlock";
+import {
+  informationLinks,
+  skills,
+} from "@/stories/components/ContentBlock/ContentBlock.stories";
+import { useRef } from "react";
 
 const navBarProps = {
   links: navLinks,
@@ -29,11 +37,40 @@ const footerProps = {
     "Nulla in velit a metus rhoncus tempus. Nulla congue nulla vel sem varius finibus. Sed ornare sit amet lorem sed viverra. In vel urna quis libero viverra facilisis ut ac est. Morbi commodo, eros in dignissim tempus, lacus odio rutrum augue, in semper sem magna quis tellus. Etiam enim erat, suscipit eu semper a, dictum sit amet elit. Nunc egestas nisi eget enim gravida facilisis. Pellentesque laoreet varius turpis vel pharetra. Ut ante justo, consequat vitae elementum tempor, accumsan nec eros. ",
 };
 
-export default async function Home() {
+export default function Home() {
+  const ref = useRef(null);
+
+  const { scrollYProgress } = useScroll();
+
+  const scaleX = useSpring(scrollYProgress, {
+    stiffness: 200,
+    damping: 30,
+    restDelta: 0.001,
+  });
+
   return (
     <div className="landing">
       <Navbar {...navBarProps} /> <Hero {...heroProps} />
       <Footer {...footerProps} />
+      <motion.section
+        ref={ref}
+        style={{
+          backgroundColor: `var(--font-beige)`,
+          padding: `var(--size-md-2)`,
+          scaleX: scaleX,
+        }}
+      >
+        <Title
+          containerPadding
+          titlePadding
+          size="large"
+          label="About me"
+          align="center"
+          border
+        />
+        <ContentBlock variant="about" information={informationLinks} />
+        <ContentBlock variant="skills" skills={skills} />
+      </motion.section>
     </div>
   );
 }
