1dbff9f028c7e7382376a13b711dd3b9d3623781
omer mohamed <damouramory@gmail.com>
2024-12-06 17:40:10 +0200

    chore: exporting test label variables

--- stories/components/Form/Form.stories.ts
+++ stories/components/Form/Form.stories.ts
@@ -15,37 +15,41 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
+export const fullLabel = [
+  { type: "text", name: "name", border: "full" },
+  { type: "number", name: "age", border: "full" },
+  { type: "text", name: "name", border: "full" },
+];
+
+export const croppedLabel = [
+  {
+    type: "text",
+    name: "Name",
+    border: "cropped",
+    required: true,
+  },
+  {
+    type: "number",
+    name: "Age",
+    border: "cropped",
+  },
+  {
+    type: "email",
+    name: "Email",
+    border: "cropped",
+  },
+];
+
 export const Full: Story = {
   args: {
-    fields: [
-      { type: "text", name: "name", border: "full" },
-      { type: "number", name: "age", border: "full" },
-      { type: "text", name: "name", border: "full" },
-    ],
+    fields: [...fullLabel],
     introText: "Text",
   },
 };
 
 export const Cropped: Story = {
   args: {
-    fields: [
-      {
-        type: "text",
-        name: "name",
-        border: "cropped",
-        required: true,
-      },
-      {
-        type: "number",
-        name: "name",
-        border: "cropped",
-      },
-      {
-        type: "email",
-        name: "name",
-        border: "cropped",
-      },
-    ],
+    fields: [...croppedLabel],
     introText: "Text",
   },
 };
