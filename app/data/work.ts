796c171a5f05620bdff09c4069e24e31d23b4077
omer mohamed <damouramory@gmail.com>
2024-12-30 12:59:35 +0200

    style tweaks

--- app/data/work.ts
+++ app/data/work.ts
@@ -1,5 +1,13 @@
-import Image from "@/assets/work-1.jpg";
+import Image1 from "@/assets/Work/site-1.png";
+import Image2 from "@/assets/Work/site-2.png";
+import Image3 from "@/assets/Work/site-3.png";
+import Image4 from "@/assets/Work/site-4.png";
+import Image5 from "@/assets/Work/site-5.png";
 
+export const Image = {
+  src: Image1,
+  alt: "Work",
+};
 export interface Project {
   link: string;
   image?: string;
@@ -9,51 +17,34 @@ export interface Project {
 
 export const myProjects: Project[] = [
   {
-    image: Image.src,
-    link: "github.com/linkbroken/d-fitness",
-    description: "D-fitness is an amazing application",
-    title: "D-fitness",
+    image: Image1.src,
+    link: "https://viu.ch",
+    description: "We help our clients to better engage in a digital world.",
+    title: "viu AG",
   },
   {
-    image: Image.src,
-    link: "github.com/linkbroken/d-fitness",
-    description: "D-fitness is an amazing application",
-    title: "D-fitness",
+    image: Image2.src,
+    link: "https://omni-food-app.vercel.app/",
+    description: "Food Application",
+    title: "Omni Food",
   },
   {
-    image: Image.src,
-    link: "github.com/linkbroken/d-fitness",
-    description: "D-fitness is an amazing application",
-    title: "D-fitness",
+    image: Image3.src,
+    link: "https://www.ag.ch/",
+    description:
+      "From the central point of contact, to job offers, legal bases, to portraits of how the canton works and its diversity, you will find the most important information here.",
+    title: "KTAG Website",
   },
   {
-    image: Image.src,
-    link: "github.com/linkbroken/d-fitness",
-    description: "D-fitness is an amazing application",
-    title: "D-fitness",
+    image: Image4.src,
+    link: "https://mahabnsahl.com/",
+    description: "A portfolio for a graphic designer",
+    title: "Maha bin Sahl",
   },
   {
-    image: Image.src,
-    link: "github.com/linkbroken/d-fitness",
-    description: "D-fitness is an amazing application",
-    title: "D-fitness",
-  },
-  {
-    image: Image.src,
-    link: "github.com/linkbroken/d-fitness",
-    description: "D-fitness is an amazing application",
-    title: "D-fitness",
-  },
-  {
-    image: Image.src,
-    link: "github.com/linkbroken/d-fitness",
-    description: "D-fitness is an amazing application",
-    title: "D-fitness",
-  },
-  {
-    image: Image.src,
-    link: "github.com/linkbroken/d-fitness",
-    description: "D-fitness is an amazing application",
-    title: "D-fitness",
+    image: Image5.src,
+    link: "https://d-yoga.vercel.app/",
+    description: "D-yoga is an application for yoga poses",
+    title: "D-yoga",
   },
 ];
